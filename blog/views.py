0ffeb8145ace8195c732b95e6520a6d186c5dd63
gal <tanjhu@163.com>
2020-01-31 16:59:37 +0800

    blog_content

--- blog/views.py
+++ blog/views.py
@@ -6,3 +6,9 @@ def blog_title(request):
     return render(request,'blog/titles.html',{'blogs':blogs})
 
 
+def blog_article(request,article_id):
+    article = BlogArticles.objects.get(id=article_id)
+    pub = article.publish
+    return render(request,'blog/content.html',{'article':article,';publish':pub})
+
+
