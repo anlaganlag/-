af49bea0855b7e4659ddad6462bbf9781828c2bf
gal <tanjhu@163.com>
2020-02-01 17:24:12 +0800

    注冊功能

--- account/urls.py
+++ account/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 app_name = 'account'
+
 urlpatterns = [
-    path('login/',views.user_login,name='user_lgoin'),
+    #path('login/',views.user_login,name='user_login'),
+    path('login/',auth_views.LoginView.as_view(template_name='account/login2.html'),name='user_login'),
+    path('logout/',auth_views.LogoutView.as_view(template_name='account/logout.html'),name='user_logout'),
+    path('register/',views.register,name='user_register'),
     #path('<int:article_id>/',views.blog_article,name='blog_article'),
     ]
