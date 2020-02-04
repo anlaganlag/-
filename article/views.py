5c0e340b511f3f3d7ac098c3175a0369ca664245
gal <tanjhu@163.com>
2020-02-04 13:58:25 +0800

    column_article_post&MD_feature

--- article/views.py
+++ article/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required 
+from django.views.decorators.http import require_POST
 from django.views.decorators.csrf import csrf_exempt 
 from django.http import HttpResponse
 
-from .models import ArticleColumn
-from .forms import ArticleColumnForm
+from .models import ArticleColumn,ArticlePost,ArticleTag
+from .forms import ArticleColumnForm, ArticlePostForm, ArticleTagForm
 
 @login_required(login_url='/account/login/') 
 @csrf_exempt
@@ -24,58 +25,60 @@ def article_column(request):
             return HttpResponse("1")
 
 
-#@login_required(login_url='/account/login') 
-#@require_POST
-#@csrf_exempt
-#def rename_article_column(request):
-    #column_name = request.POST["column_name"] 
-    #column_id = request.POST['column_id'] 
-    #try:
-        #line = ArticleColumn.objects.get(id=column_id) 
-        #line.column = column_name 
-        #line.save()
-        #return HttpResponse("1")
-    #except:
-        #return HttpResponse("0")
-#
-#@login_required(login_url='/account/login') 
-#@require_POST
-#@csrf_exempt
-#def del_article_column(request):
-    #column_id = request.POST["column_id"] 
-    #try:
-        #line = ArticleColumn.objects.get(id=column_id) 
-        #line.delete()
-        #return HttpResponse("1") 
-    #except:
-        #return HttpResponse("2")
-#
-#@login_required(login_url='/account/login') 
-#@csrf_exempt
-#def article_post(request):
-    #if request.method=="POST":
-        #article_post_form = ArticlePostForm(data=request.POST) 
-        #if article_post_form.is_valid():
-            #cd = article_post_form.cleaned_data 
-            #try:
-                #new_article = article_post_form.save(commit=False) 
-                #new_article.author = request.user
-                #new_article.column = request.user.article_column.get(id=request.POST['column_id'])
-                #new_article.save()
+@login_required(login_url='/account/login') 
+@require_POST
+@csrf_exempt
+def rename_article_column(request):
+    column_name = request.POST["column_name"] 
+    column_id = request.POST['column_id'] 
+    try:
+        line = ArticleColumn.objects.get(id=column_id) 
+        line.column = column_name 
+        line.save()
+        return HttpResponse("1")
+    except:
+        return HttpResponse("0")
+
+@login_required(login_url='/account/login') 
+@require_POST
+@csrf_exempt
+def del_article_column(request):
+    column_id = request.POST["column_id"] 
+    try:
+        line = ArticleColumn.objects.get(id=column_id) 
+        line.delete()
+        return HttpResponse("1") 
+    except:
+        return HttpResponse("2")
+
+
+@login_required(login_url='/account/login')
+@csrf_exempt
+def article_post(request):
+    if request.method=="POST":
+        article_post_form = ArticlePostForm(data=request.POST) 
+        if article_post_form.is_valid():
+            cd = article_post_form.cleaned_data
+            try:
+                new_article = article_post_form.save(commit=False) 
+                new_article.author = request.user
+                new_article.column = request.user.article_column.get(id=request.POST['column_id'])
+                new_article.save()
                 #tags = request.POST['tags']
                 #if tags:
                     #for atag in json.loads(tags):
                         #tag = request.user.tag.get(tag=atag)
                         #new_article.article_tag.add(tag)
-                #return HttpResponse("1") 
-            #except:
-                #return HttpResponse("2") 
-        #else:
-            #return HttpResponse("3")
-    #else:
-        #article_post_form = ArticlePostForm()
-        #article_columns = request.user.article_column.all()
+                return HttpResponse("1") 
+            except:
+                return HttpResponse("2") 
+        else:
+            return HttpResponse("3")
+    else:
+        article_post_form = ArticlePostForm()
+        article_columns = request.user.article_column.all()
         #article_tags = request.user.tag.all()
+        return render(request, "article/column/article_post.html",{"article_post_form":article_post_form, "article_columns":article_columns})
         #return render(request, "article/column/article_post.html",{"article_post_form":article_post_form, "article_columns":article_columns, "article_tags":article_tags})
 #
 #@login_required(login_url='/account/login') 
--- myproject/forms.py
+++ myproject/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import ArticleColumn, ArticlePost
+from .models import ArticleColumn, ArticlePost, Comment
 from .models import ArticleTag
 
 class ArticleColumnForm(forms.ModelForm): 
@@ -12,11 +12,11 @@ class ArticlePostForm(forms.ModelForm):
         model = ArticlePost 
         fields = ("title", "body")
 
-#
-#class CommentForm(forms.ModelForm): 
-    #class Meta:
-        #model = Comment
-        #fields = ("commentator", "body",)
+
+class CommentForm(forms.ModelForm): 
+    class Meta:
+        model = Comment
+        fields = ("commentator", "body",)
 
 class ArticleTagForm(forms.ModelForm):
     class Meta:
