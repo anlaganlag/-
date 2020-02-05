44c54c2078d64cf9fde97ef64454d9ec70370fad
gal <tanjhu@163.com>
2020-02-05 19:44:21 +0800

    展示專欄/作者專欄加入對因userinfo

--- article/list_views.py
+++ article/list_views.py
@@ -7,8 +7,13 @@ def article_titles(request,username=None):
     if username:
         user = User.objects.get(username=username)
         articles_title = ArticlePost.objects.filter(author=user)
+        try:
+            userinfo = user.userinfo
+        except:
+            userinfo = None
     else:
         articles_title = ArticlePost.objects.all()
+
     paginator = Paginator(articles_title,5)
     page = request.GET.get('page')
     try:
@@ -20,8 +25,9 @@ def article_titles(request,username=None):
     except EmptyPage:
         current_page = paginator.page(paginator.num_pages)
         articles = current_page.object_list
+
     if username:
-        return render(request,'article/list/author_articles.html', {'articles':articles,'page':current_page})
+        return render(request,'article/list/author_articles.html', {'articles':articles,'page':current_page,'userinfo':userinfo,'user':user})
     return render(request,'article/list/article_titles.html', {'articles':articles,'page':current_page})
 
 def article_detail(request,id,slug):
