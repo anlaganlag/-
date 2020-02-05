9d5d6cf4979f882d177173c403773918eb35a09d
gal <tanjhu@163.com>
2020-02-05 21:37:31 +0800

    修復css無效bug

--- article/models.py
+++ article/models.py
@@ -29,7 +29,7 @@ class ArticlePost(models.Model):
     body = models.TextField()
     created = models.DateTimeField(default=timezone.now)
     updated = models.DateTimeField(auto_now=True)
-    #users_like = models.ManyToManyField(User, related_name="articles_like", blank=True)
+    users_like = models.ManyToManyField(User, related_name="articles_like", blank=True)
     #article_tag = models.ManyToManyField(ArticleTag, related_name='article_tag', blank=True)
     class Meta:
         ordering = ("-updated",) 
